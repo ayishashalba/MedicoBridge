6799907d4ef5a7e749fedc2cbc1a0fd9b14783da
Ayisha Shalba <ayishashalba555@gmail.com>
2026-07-15 20:31:23 +0530

    landing completed

--- frontend/src/pages/Landing/Landing.jsx
+++ frontend/src/pages/Landing/Landing.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import HeroSection from "../../components/HeroSection/HeroSection";
-import SearchSection from "../../components/SearchSection/SearchSection";
 import JoinSection from "../../components/JoinSection/JoinSection";
 import WhyChoose from "../../components/WhyChoose/WhyChoose";
 import TopDoctors from "../../components/TopDoctors/TopDoctors";
@@ -17,9 +16,6 @@ function Landing() {
       <main>
         <section id="hero">
           <HeroSection />
-        </section>
-        <section id="search">
-          <SearchSection />
         </section>
         <section id="join">
           <JoinSection />
